53c9a30eb3bdb0f7e9f87e5b573ab63eabb6dc61
Larry Hatcer <actswolf@gmail.com>
2021-08-18 20:31:36 -0500

    Created new variables that random the 4 main topics and then printed to console log to test. Everything is good all array items came up with no errors.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 "use strict"
 
-let ranDest = ["Florda", "Japan", "Hawaii", "California"]
-let ranResturant = ["Steak", "Seafood", "PHO", "BBQ"]
-let ranTransport = ["Bike", "Car", "Truck", "SUV"]
-let ranEntertainment = ["Live Music", "Fire Works", "Guided Tour", "Museum"]
+let ranDest = ["Florda", "Japan", "Hawaii", "California"];
+let ranResturant = ["Steak", "Seafood", "PHO", "BBQ"];
+let ranTransport = ["Bike", "Car", "Truck", "SUV"];
+let ranEntertainment = ["Live Music", "Fire Works", "Guided Tour", "Museum"];
 
+let dest = ranDest[Math.floor(Math.random() * ranDest.length)];
+let resturant = ranResturant[Math.floor(Math.random() * ranResturant.length)];
+let tran = ranTransport[Math.floor(Math.random() * ranTransport.length)];;
+let entertain = ranEntertainment[Math.floor(Math.random() * ranEntertainment.length)];
+
+// let count = "";
+
+// function getRandomSelection(ranDest){
+//     count = str[Math.floor(Math.random() * str.length)];
+//     return dest
+// }    
+// getRandomSelection(ranDest);
+console.log(dest);
+console.log(resturant);
+console.log(tran);
+console.log(entertain);
