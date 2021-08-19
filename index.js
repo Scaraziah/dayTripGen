43262eb2c4060ddbd82d55d7f3b5759c6ad25693
Larry Hatcer <actswolf@gmail.com>
2021-08-18 19:43:42 -0500

    Created 4 main arrays to be randomly selected.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 "use strict"
+
+let ranDest = ["Florda", "Japan", "Hawaii", "California"]
+let ranResturant = ["Steak", "Seafood", "PHO", "BBQ"]
+let ranTransport = ["Bike", "Car", "Truck", "SUV"]
+let ranEntertainment = ["Live Music", "Fire Works", "Guided Tour", "Museum"]
+
