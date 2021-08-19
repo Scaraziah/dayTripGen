0372e5581bb28e08df03aca184c241d25fbf8cc6
Larry Hatcer <actswolf@gmail.com>
2021-08-18 21:47:49 -0500

    Started if statments for if they are liking everything or not. Created promt varables that yes and know and the answers inserting the varables in a sentance.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let ranDest = ["Florda", "Japan", "Hawaii", "California"];
 let ranResturant = ["steak", "seafood", "PHO", "BBQ"];
 let ranTransport = ["bike", "car", "truck", "SUV"];
-let ranEntertainment = ["boat ride", "lights and fire works show", "rock consert", "guided museum tour"];
+let ranEntertainment = ["race", "lights and fire works show", "rock consert", "guided museum tour"];
 
 let dest = ranDest[Math.floor(Math.random() * ranDest.length)];
 let resturant = ranResturant[Math.floor(Math.random() * ranResturant.length)];
@@ -21,3 +21,20 @@ let myTrip = ("Your destination will be in " + dest + " with a mode of transport
 resturant + " and the entertainment will be a " + entertain + ".");
 
 console.log(myTrip);
+
+let isTripAcceptable = parseInt(prompt("Is the trip acceptable for you? Press 1 for YES and 2 for NO."));
+console.log(isTripAcceptable);
+let yesOrNo = parseInt(prompt("Press 1 for YES and 2 for NO."));
+
+if(isTripAcceptable !== 2){
+    console.log("That's not good. Did you not like the destination?");
+    console.log(yesOrNo)
+    if(yesOrNo !==2){
+
+    }
+}    
+else{
+    console.log("That's great. Hope you have a wonderful time at the " + entertain + ". " + dest + 
+    " is a wonderful place and by using the " + tran + " you will be able to get between eating " + resturant + " and the " + entertain + 
+    " easily.");
+}
