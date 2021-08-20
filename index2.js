67d22f327cd1c12cffd7ff00e1dddd9957bcfc8c
Larry Hatcer <actswolf@gmail.com>
2021-08-19 21:17:21 -0500

    Cleaned up code. Removed varaibles and added ; where they needed to be. Created backup of original code that worked.

--- index2.js
+++ index2.js
@@ -31,13 +31,13 @@ function happyCheck(){
     if(isTripAcceptable == 2){
         if(mainMenus == "destination"){
             dest = ranDest[Math.floor(Math.random() * ranDest.length)];
-            console.log("Is " + dest + " good for you??");
-            yesOrNo = prompt("Yes or No");
+            console.log("Is " + dest + " good for you??")
+            yesOrNo = prompt("Yes or No")
            
             switch(yesOrNo){
                 case "Yes":
                     isTripAcceptable = parseInt(prompt("Great! Your new trip will be in " + dest + " with a mode of transportaion of a " + tran + ". The resturant will be serving " + 
-                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."));
+                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."))
                     break;
                 case "No":
                     dest = ranDest[Math.floor(Math.random() * ranDest.length)];
@@ -73,7 +73,7 @@ function happyCheck(){
             switch(yesOrNo){
                 case "Yes":
                     isTripAcceptable = parseInt(prompt("Your new trip will be in " + dest + " with a mode of transportaion of a " + tran + ". The resturant will be serving " + 
-                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."));
+                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."))
                     break;
                 case "No":
                     resturant = ranResturant[Math.floor(Math.random() * ranResturant.length)];
@@ -86,17 +86,17 @@ function happyCheck(){
         }
         else if(mainMenus == "entertainment"){
             entertain = ranEntertainment[Math.floor(Math.random() * ranEntertainment.length)];
-            console.log("Is " + entertain + " good for you??");
-            yesOrNo = prompt("Yes or No");
+            console.log("Is " + entertain + " good for you??")
+            yesOrNo = prompt("Yes or No")
             switch(yesOrNo){
                 case "Yes":
                     isTripAcceptable = parseInt(prompt("Your new trip will be in " + dest + " with a mode of transportaion of a " + tran + ". The resturant will be serving " + 
-                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."));
+                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."))
                     break;
                 case "No":
                     entertain = ranEntertainment[Math.floor(Math.random() * ranEntertainment.length)];
                     isTripAcceptable = parseInt(prompt("Your new trip will be in " + dest + " with a mode of transportaion of a " + tran + ". The resturant will be serving " + 
-                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."));
+                    resturant + " and the entertainment will be a " + entertain + ". Is the trip acceptable for you? Press 1 for YES and 2 for NO."))
                     break;
                 default :
             }
@@ -111,3 +111,58 @@ console.log("This is going to be a great day trip. Hope you have a wonderful tim
 " is a wonderful place and by using the " + tran + " you will be able to get between eating " + resturant + " and the " + entertain + 
 " easily.");  
     
+
+ // if(yesOrNo == "No"){
+            //     dest = ranDest[Math.floor(Math.random() * ranDest.length)];
+            //     console.log("Is " + dest + " good for you??")
+            //     yesOrNo = prompt("Yes or No")
+            // }
+
+    //     else if(mainMenus == "entertainment"){
+    //         entertain = ranEntertainment[Math.floor(Math.random() * ranEntertainment.length)];
+    //         console.log("Is " + entertain + " good for you?")
+    //     }
+    //     else if(mainMenus == "transportaion"){
+    //         tran = ranTransport[Math.floor(Math.random() * ranTransport.length)];
+    //         console.log("Is " + tran + " good for you?")
+    //     }
+    //     else if(mainMenus == "resturant"){
+    //         resturant = ranResturant[Math.floor(Math.random() * ranResturant.length)];
+    //         console.log("Is " + resturant + " good for you?")
+    //     }
+    // }    
+    //     else{
+
+// switch(mainMenus){
+//     case "Destination":
+//         console.log(fixThat);
+//         getRandomSelection(dest);
+//         console.log("Is " + dest + " good for you?");
+//         break;
+//     case "Transportaion":
+//         console.log(fixThat);
+//         getRandomeSelection(dest);
+//         console.log("Is " + dest + " good for you?");
+//         break;   
+// }
+
+
+
+    // console.log("That's not good. What did you not like? 1. ");
+    // console.log(yesOrNo)
+    // if(yesOrNo == 2){
+    //     console.log("Lets fix that.")
+    // switch(mainMenus){
+    // case "destination":
+    //     // console.log(fixThat);
+    //     getRandomeSelection(dest);
+    //     count = dest
+    //     console.log("Is " + dest + " good for you?");
+    //     break;
+    // case "transportaion":
+    //     console.log(fixThat + dest);
+    //     getRandomeSelection(dest);
+    //     console.log("Is " + dest + " good for you?");
+    //     break; 
+    // }    
+    // } 
